5be440b2af83d72aa2137c634f168a89c806ed1d
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-14 01:46:22 -0500

    Add new implementation of RoundNumber

--- src/main/java/com/developersdelicias/util/RoundNumberUtil.java
+++ src/main/java/com/developersdelicias/util/RoundNumberUtil.java
@@ -1,17 +1,15 @@
 package com.developersdelicias.util;
 
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class RoundNumberUtil {
 
     public static double round(double numberToRound, int numberOfDecimals) {
-        BigDecimal bd = new BigDecimal(numberToRound).setScale(numberOfDecimals, RoundingMode.HALF_EVEN);
-        return bd.doubleValue();
+        double multiplier = Math.pow(10, numberOfDecimals);
+        return (double)Math.round(numberToRound * multiplier) / multiplier;
     }
 
     public static double roundToFourDecimals(double numberToRound) {
         return round(numberToRound, 4);
     }
+
 }
--- src/test/java/com/developersdelicias/util/RoundNumberUtilTest.java
+++ src/test/java/com/developersdelicias/util/RoundNumberUtilTest.java
@@ -1,39 +1,37 @@
 package com.developersdelicias.util;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static com.developersdelicias.util.RoundNumberUtil.round;
 import static com.developersdelicias.util.RoundNumberUtil.roundToFourDecimals;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class RoundNumberUtilTest {
 
     @Test
-    public void should_round_number_to_four_digits() throws Exception {
-        double numberToRound = 5.78968d;
+    @Parameters({"5.78965d", "5.78966d", "5.78967d", "5.78968d", "5.78969d"})
+    public void should_round_number_to_four_digits_up(double numberToRound) throws Exception {
         double expectedNumber = 5.7897d;
-        assertThat(round(numberToRound, 4), is(expectedNumber));
+        assertThat(roundToFourDecimals(numberToRound), is(expectedNumber));
     }
 
     @Test
-    public void should_round_number_to_four_digits2() throws Exception {
-        double numberToRound = 5.78961d;
-        double expectedNumber = 5.7896d;
-        assertThat(round(numberToRound, 4), is(expectedNumber));
-    }
-
-    @Test
-    public void should_round_number_to_four_digits3() throws Exception {
-        double numberToRound = 5.78962d;
-        double expectedNumber = 5.7896d;
-        assertThat(round(numberToRound, 4), is(expectedNumber));
-    }
-
-    @Test
-    public void should_round_number_to_four_digits4() throws Exception {
-        double numberToRound = 5.78962d;
+    @Parameters({"5.78960d", "5.78961d", "5.78962d", "5.78963d", "5.78964d"})
+    public void should_round_number_to_four_digits_down(double numberToRound) throws Exception {
         double expectedNumber = 5.7896d;
         assertThat(roundToFourDecimals(numberToRound), is(expectedNumber));
+    }
+
+    @Test
+    public void should_round_numbers_with_different_number_of_decimals() throws Exception {
+        assertThat(round(10.2365869, 2), is(10.24));
+        assertThat(round(10.2365869, 7), is(10.2365869));
+        assertThat(round(10.2365869, 0), is(10.0));
+        assertThat(round(10.2365869, 8), is(10.2365869));
     }
 }
